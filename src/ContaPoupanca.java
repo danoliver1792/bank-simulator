d53a098d971d8944ce16b068ee11af512f91cebc
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-09-29 22:55:51 -0300

    adicionando getters e setters

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,9 +1,9 @@
 
 public class ContaPoupanca {
 	
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 	
 	public void sacar() {
 		
@@ -16,4 +16,19 @@ public class ContaPoupanca {
 	public void transferir() {
 		
 	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
+	
 }
